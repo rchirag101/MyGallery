37ca6ec6e631a2515faa2cffa8bd850afef254d4
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2018-10-15 15:57:21 +0530

    Pinch zoom Gesture added
    Zoom in/out image by pinch Gesture

--- src/main/java/com/example/chirag/mygallery/SingleViewActivity.java
+++ src/main/java/com/example/chirag/mygallery/SingleViewActivity.java
@@ -2,15 +2,21 @@ package com.example.chirag.mygallery;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by chirag on 13-10-2018.
- */
-
 public class SingleViewActivity extends Activity{
+
+    private ScaleGestureDetector msgd;
+    private float mScaleFactor=1.0f;
+
+    ImageView imageView=null;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_view);
@@ -21,8 +27,30 @@ public class SingleViewActivity extends Activity{
         int position = i.getExtras().getInt("id");
         setTitle(position+".jpg");
         ImageAdapter imageAdapter = new ImageAdapter(this);
-        ImageView imageView = (ImageView) findViewById(R.id.SingleView);
+        imageView = (ImageView) findViewById(R.id.SingleView);
         imageView.setImageResource(imageAdapter.mThumbIds[position]);
-        Toast.makeText(this,"id : "+imageAdapter.mThumbIds[position].toString(), Toast.LENGTH_SHORT).show(); //toast
+        Toast.makeText(this,"id : "+imageAdapter.mThumbIds[position].toString(), Toast.LENGTH_SHORT).show();
+
+        msgd = new ScaleGestureDetector(this,new ScaleListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        msgd.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    //inner class to handle Pinch Gesture
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor  = Math.max(0.1f,Math.min(mScaleFactor,50.0f));
+            
+            imageView.setScaleX(mScaleFactor);
+            imageView.setScaleY(mScaleFactor);
+
+            return true;
+        }
     }
 }
